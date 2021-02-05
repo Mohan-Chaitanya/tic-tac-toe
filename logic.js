d5f12c6d0fb7982661576aec4f44dd1d4b51836b
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-02-05 02:42:44 -0500

    added-styling

---
+++ logic.js
@@ -0,0 +1,4 @@
+var gameBoard = ['O', 'X', 'O', 'X', 'O', 'O', 'O', 'X', 'O'];
+
+var playerMark = document.querySelector('#gameCell1');
+playerMark.addEventListener('click', () => { console.log('Clicked') });
